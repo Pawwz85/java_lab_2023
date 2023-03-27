a9c5469bf48ebf8731094788ce45623587179443
student <pawelwzietek@gmail.com>
2023-03-27 16:43:59 +0200

    Second Project

---
+++ Streams/Main.java
@@ -0,0 +1,15 @@
+import java.nio.file.Path;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        FileCommander fileCommander =new FileCommander();
+        System.out.println(fileCommander.pwd());
+        fileCommander.cd(Path.of("/home/Student/Pulpt"));
+        System.out.println(fileCommander.pwd());
+        fileCommander.cd(Path.of("..").normalize());
+        System.out.println(fileCommander.pwd());
+        fileCommander.cd(Path.of("..").normalize());
+    }
+}
